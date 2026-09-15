67305209e268048c5d1c1a82c881e25da7a151a5
yomZsamora <samaurah@gmail.com>
2026-09-15 17:47:16 +0300

    Refactor custom exceptions to consolidate error handling into a base AppError class, simplifying the structure and adding operational properties.

--- src/utils/exceptions/custom-exceptions.js
+++ src/utils/exceptions/custom-exceptions.js
@@ -1,77 +1,33 @@
-'use strict';
-
-class BadRequest extends Error {
-    constructor(message, errors = null) {
+class AppError extends Error {
+    constructor(message, statusCode) {
         super(message);
-        this.name = 'BadRequest';
-        this.errors = errors;
-        this.statusCode = 400;
+        this.statusCode = statusCode;
+        this.isOperational = true;
+        Error.captureStackTrace(this, this.constructor);
     }
 }
 
-class NotFound extends Error {
-    constructor(message) {
-        super(message);
-        this.name = 'NotFound';
-        this.statusCode = 404;
+class BadRequest extends AppError {
+    constructor(message = 'Bad Request') {
+        super(message, 400);
     }
 }
 
-class Conflict extends Error {
-    constructor(message) {
-        super(message);
-        this.name = 'Conflict';
-        this.statusCode = 409;
+class NotFound extends AppError {
+    constructor(message = 'Not Found') {
+        super(message, 404);
     }
 }
 
-class NotAuthenticated extends Error {
-    constructor() {
-        super('Authentication credentials were not provided.');
-        this.statusCode = 401;
-        this.name = 'NotAuthenticated';
+class Conflict extends AppError {
+    constructor(message = 'Conflict') {
+        super(message, 409);
     }
 }
 
-class PermissionDenied extends Error {
-    constructor() {
-        super("You don't have required permission to perform this action.");
-        this.statusCode = 403;
-        this.name = 'PermissionDenied';
-    }
-}
-
-class TokenExpired extends Error {
-    constructor() {
-        super('Token has expired.');
-        this.statusCode = 401;
-        this.name = 'TokenExpired';
-    }
-}
-
-class InvalidJsonWebToken extends Error {
-    constructor() {
-        super('Provided token is invalid.');
-        this.statusCode = 401;
-        this.name = 'InvalidJsonWebToken';
-    }
-}
-
-class TokenReuseDetected extends Error {
-    constructor() {
-        super('Token reuse detected. All sessions have been revoked.');
-        this.statusCode = 401;
-        this.name = 'TokenReuseDetected';
-    }
-}
-
-module.exports = {
-    BadRequest,
-    NotFound,
-    Conflict,
-    NotAuthenticated,
-    PermissionDenied,
-    TokenExpired,
-    InvalidJsonWebToken,
-    TokenReuseDetected,
+module.exports = { 
+    AppError, 
+    BadRequest, 
+    NotFound, 
+    Conflict 
 };
